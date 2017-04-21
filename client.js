d6588c7fc4d5017740f2c4631ac498dd6eda8014
Jack Preston <unwttng@gmail.com>
2017-04-21 15:07:41 +0100

    Cleaning up when the server drops the game or someone wins it

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ const { Board } = require('hive-game-core')
 const request = require('request-promise-native')
 const uuid = require('uuid/v4')
 
-const GAME_HOST = 'http://prod-hive-game-server-1259273739.eu-west-1.elb.amazonaws.com'
+const GAME_HOST = 'http://localhost:8000'
 
 class Client {
 
@@ -25,30 +25,36 @@ class Client {
 
   async joinFreeGame() {
     this.log(`_Joining free game_`)
-    const { token } = await this._request(`${GAME_HOST}/game/free`)
-    this.log(`_Joined_ free game with token **${token}**`)
-    this._games.push(token)
-    return token
+    const { gameId } = await this._request(`${GAME_HOST}/game/new`)
+    this.log(`_Joined_ free game with gameId **${gameId}**`)
+    this._games.push(gameId)
+    return gameId
   }
 
   async processOneMove() {
     this.log(`_Processing one move_`)
-    const statuses = await Promise.all(_.map(this._games, token => this._request(`${GAME_HOST}/game/status/${token}`)))
+    const statuses = await Promise.all(_.map(this._games, gameId => this._request(`${GAME_HOST}/game/${gameId}/status`)))
     this.log(`_Got statuses_ for **${statuses.length}** games`)
+    const toRemove = []
     const statusesToMove = _.filter(statuses, s => {
+      if (s.status === 'NONEXISTENT') {
+        toRemove.push(s.gameId)
+        this.log(`_Something went wrong_ and the server seems not to know about game **${s.gameId}**, removing from my list`)
+      }
       if (!s.state) { return false }
-      if (s.state.gameOver) {
-        this._games = _.reject(this._games, token => token === s.token)
-        this.log(`_Game over_ for game **${s.token}**, removing from my list`)
+      if (s.status === 'COMPLETED' || s.state.gameOver) {
+        toRemove.push(s.gameId)
+        this.log(`_Game over_ for game **${s.gameId}**, removing from my list`)
         return false
       }
       return s.state.toMove === this._id
     })
+    this._games = _.reject(this._games, gameId => _.includes(toRemove, gameId))
     this.log(`_Found_ **${statusesToMove.length}** games waiting for my move`)
     if (statusesToMove.length === 0) { return }
     const chosenGame = statusesToMove[_.random(statusesToMove.length - 1)]
     const move = this.move(chosenGame.state)
-    await this._request(`${GAME_HOST}/game/${chosenGame.token}/move/${move}/${chosenGame.hash}`)
+    await this._request(`${GAME_HOST}/game/${chosenGame.gameId}/move/${move}/${chosenGame.hash}`)
   }
 
   move(gameState) { throw 'move() not implemented' }
